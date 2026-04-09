0c65aa301b6732be233d61acec2ed93f74b0ffec
Emmanuellekan <84884833+Emmanuellekan@users.noreply.github.com>
2026-04-09 23:52:43 +0100

    Add files via upload

--- task_tracker.py
+++ task_tracker.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import json
 
@@ -9,69 +9,72 @@ class Task:
     description : str
     status : str
     created_on : datetime
-    mark_done : datetime
+    end_date : datetime
     updated_on: datetime
     DATE_FORMAT = '%y/%m/%d - %H:%M:%S'
 
 
-    def __init__(self, id, task, description, status, created_on, mark_done):
+    def __init__(self, id, task, description, is_done, status, created_on, end_date, done_at):
         self.id = id
         self.task = task
         self.description = description
+        self.is_done = is_done
         self.status = status
         self.created_on = created_on
-        self.mark_done = datetime.strptime(mark_done, self.DATE_FORMAT)
-        
+        self.end_date = datetime.strptime(end_date, self.DATE_FORMAT)
+        self.done_at = done_at
 
     def to_json(self):
         return {
             'id' : self.id,
             'task' : self.task,
             'description' : self.description,
+            'is_done' : False,
             'status' : self.status,
             'created_on' : self.created_on,
-            'mark_done' : self.mark_done.strftime(self.DATE_FORMAT),
+            'end_date' : self.end_date.strftime(self.DATE_FORMAT),
+            'done_at': self.done_at,
             'updated_on': None
         }
     
 
 class TaskManager:
     FILENAME = 'task_tracker.json'
-    # DATE_FORMAT = '%y/%m/%d - %H:%M:%S'
+    DATE_FORMAT = '%y/%m/%d - %H:%M:%S'
 
     def __init__(self):
         self.tasks = self.load_task()
         self.status = 'To-do'
-        # self.updated = 'Not updated'
 
         for i, task in enumerate(self.tasks):
-            created_on = datetime.strptime(task['created_on'], '%d/%m/%y %H:%M:%S')
-            mark_done = datetime.strptime(task['mark_done'], '%y/%m/%d - %H:%M:%S')
+            created_on = datetime.strptime(task['created_on'], self.DATE_FORMAT)
+            end_date = datetime.strptime(task['end_date'], self.DATE_FORMAT)
             now = datetime.now()
 
 
             # print(f'the date is {created_on.date()}')
-            if created_on == now:
-                task['status'] = 'To-do'
-                
-
-            elif now < mark_done and now > created_on:
-                task['status'] = 'In-process'
-
-                # print(f'task expired {now}/ {mark_done} {self.status} ~ { i + 1}')
-            else:
+            if task['is_done']:
                 task['status'] = 'Done'
-                # print(f'not expired {now}/ {mark_done} {self.status} ~ { i + 1}')
+                print(f'status is {task['status']}')
 
+            elif created_on == now:
+                task['status'] = 'To-do'
+                print(f'status is {task['status']} todo')
+            
+            elif now > end_date:
+                task['status'] = 'Expired'
+                print(f'status is {task['status']}')
+            
+            else:
+                task['status'] = 'In-process'
+                print(f'status is {task['status']}')
         
-            
-
-            
-
-
 
     def load_task(self):
         if not os.path.exists(self.FILENAME):
+            return []
+        
+        if os.path.getsize(self.FILENAME) == 0:
             return []
         
         else:
@@ -90,17 +93,43 @@ class TaskManager:
             task_id = len(self.tasks) + 1
             task = input('Enter task: ')
             description = input('Enter description: ')
-            status = self.status
-            create_on = datetime.now().strftime('%d/%m/%y %H:%M:%S')
-            mark_done = input('Enter date to expire in this format(DD/MM/YY - H/M/S): ')
 
- 
-            tasks = Task(task_id, task, description, status, create_on, mark_done)
+            is_done = False
+
+            status = self.status
+            create_on = datetime.now().strftime(self.DATE_FORMAT)
+
+            end_date = input('Enter date to expire in this format(YY/MM/DD - H/M/S): ')
+            # days = int(input("Enter number of days to complete task: "))
+            # end_date = (datetime.now() + timedelta(days=days)).strftime('%d/%m/%y %H:%M:%S')
+
+            done_at = None
+
+
+            tasks = Task(task_id, task, description, is_done, status, create_on, end_date, done_at)
 
             self.tasks.append(tasks.to_json())
             
         except (Exception, KeyboardInterrupt) as e:
             print('Error adding task or interrupted by user: ', e)
+
+        self.save_task(self.tasks)
+
+
+    def mark_done(self) :
+
+        task_id = int(input('which number of task should be marked done: '))
+
+        mark = input('done(true) or not(false): ')
+
+        task = self.tasks[task_id - 1]
+        if mark == 'true':
+            task['is_done'] = True
+        elif mark == 'false':
+            task['is_done'] = False
+
+
+        task['done_at'] = datetime.now().strftime(self.DATE_FORMAT)
 
         self.save_task(self.tasks)
 
@@ -118,7 +147,7 @@ class TaskManager:
         new_task = input('Enter new task: ')
         new_description = input('Enter new description: ')
         new_expired_date = input('Enter date to expire in this format(YY/MM/DD - H/M/S): ')
-        new_status = self.status
+        # new_status = 
 
         if new_task:
             self.tasks[task_id - 1]['task'] = new_task
@@ -127,10 +156,10 @@ class TaskManager:
             self.tasks[task_id - 1]['description'] = new_description
 
         if new_expired_date:
-            self.tasks[task_id - 1]['mark_done'] = new_expired_date
+            self.tasks[task_id - 1]['end_date'] = new_expired_date
         
-        if new_status:
-            self.tasks[task_id - 1]['status'] = new_status
+        # if new_status:
+        #     self.tasks[task_id - 1]['status'] = new_status
 
         self.tasks[task_id - 1]['updated_on'] = datetime.now().strftime('%d/%m/%y %H:%M:%S')
 
@@ -153,20 +182,23 @@ class TaskManager:
         self.save_task(self.tasks)
 
 taskmanager = TaskManager()
-
+# taskmanager.mark_done()
 while True:
     print('\n')
     print('1. Add task')
-    print('2. Update task')
-    print('3. Delete task')
-    print('4. View task')
-    print('5. Exit')
+    print('2. Mark Task')
+    print('3. Update task')
+    print('4. Delete task')
+    print('5. View task')
+    print('6. Exit')
 
     try:
         choice = input('Enter -t(task) command follow by action to take: ')
 
         if choice == '-t add':
             taskmanager.add_task()
+        elif choice == '-t mark':
+            taskmanager.mark_done()
         elif choice == '-t update':
             taskmanager.update_task()
         elif choice == '-t delete':
@@ -174,7 +206,16 @@ while True:
         elif choice == '-t view':
             tasks = taskmanager.tasks
             for task in tasks:
-                print(f'id: {task['id']} - task: {task['task']} - description: {task['description']} - status: {task['status']} - created_on: {task['created_on']}')
+                print(f"""
+                ID: {task['id']}
+                Task: {task['task']}
+                Description: {task['description']}
+                Status: {task['status']}
+                Created: {task['created_on']}
+                Ends: {task['end_date']}
+                -------------------------
+                """)
+                
         elif choice == '-t exit':
             print('Good bye')
             break
